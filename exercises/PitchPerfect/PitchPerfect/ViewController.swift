bf5fe729278fb967400b57aa5d678e6549204d47
jsferrer1 <jsferrer1@yahoo.com>
2020-06-01 20:25:52 +0800

    Adds the UI auto-layout with constraints

---
+++ exercises/PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Jerry Ferrer on 6/1/20.
+//  Copyright © 2020 Home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordBtn: UIButton!
+    @IBOutlet weak var stopRecordingBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        stopRecordingBtn.isEnabled = false;
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        print("record button was pressed.")
+        recordingLabel.text = "Recording in Progress"
+        recordBtn.isEnabled = false;
+        stopRecordingBtn.isEnabled = true;
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabel.text = "Tap to Record"
+        recordBtn.isEnabled = true;
+        stopRecordingBtn.isEnabled = false;
+    }
+}
+
---
+++ learn-swift-programming-master/Variables and Types.playground/Pages/Your First Lines of Swift.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: ## Your First Lines of Swift
+//: Example 1
+var question = "Ready to write your first lines of Swift code?"
+question = "x"
+print(question)
+//: Example 2
+var response = "Yeah! I'm ready!"
+print(response)
+//: [Next](@next)
